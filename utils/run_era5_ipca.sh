ec04b658c944b901afcc2ae93124e43b041eba25
Ting-Shuo Yo <tingshuo.yo@googlemail.com>
2021-04-23 10:47:41 +0800

    dified shell scripts

--- utils/run_era5_ipca.sh
+++ utils/run_era5_ipca.sh
@@ -6,7 +6,8 @@
 SRCPATH="../data/era5/"
 OUTPATH="./"
 
-VARS=("u200" "v200" "h500" "q700" "t700" "u700" "v700" "q850" "t850" "u850" "v850" "q925" "t925" "u925" "v925")
+#VARS=("u200" "v200" "h500" "q700" "t700" "u700" "v700" "q850" "t850" "u850" "v850" "q925" "t925" "u925" "v925")
+VARS=("mslp")
 
 for i in "${!VARS[@]}"; do
     INPUT=$SRCPATH/"${VARS[$i]}"
